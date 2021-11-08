9da224fb0820ffcd488ff10d4b78e2aa46dc927a
Ramsai0138 <93178661+Ramsai0138@users.noreply.github.com>
2021-11-08 12:07:52 +0530

    Add files via upload

---
+++ RAMSAI TRUE OR FALSE.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+   int a,b;
+   a=20;
+   b=10;
+   
+   printf("a=%d\nb=%d\n",a,b);
+   
+   printf(" %d < %d --> %d\n",a,b,a<b);
+   
+   printf(" %d > %d --> %d\n",a,b,a>b);
+   
+   printf(" %d <= %d --> %d\n",a,b,a<=b);
+   
+   printf(" %d >= %d --> %d\n",a,b,a>=b);
+   
+   printf(" %d == %d --> %d\n",a,b,a==b);
+   
+   printf(" %d != %d --> %d",a,b,a!=b);
+   
+   
+   
+   	
+	
+	
+	
+	
+}
