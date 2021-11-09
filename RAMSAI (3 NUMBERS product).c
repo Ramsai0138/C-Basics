2fbaad2f133bd0ed7aba91ac2965c67f4a383f6a
Ramsai0138 <93178661+Ramsai0138@users.noreply.github.com>
2021-11-09 12:18:34 +0530

    Add files via upload

---
+++ RAMSAI (3 NUMBERS product).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("Enter Four Numbers : ");
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+	
+	printf("Number 1 is : %d\n",a);
+	printf("Number 2 is : %d\n",b);
+	printf("Number 3 is : %d\n",c);
+	printf("Number 4 is : %d\n",d);
+	
+	int n = a*b*c*d;
+	
+	printf("Product of 4 Numbers are : %d",n);
+	
+	
+	
+	
+	
+}
